2a3f6dfda7296413773d78caaa0e72668eed5352
AtiqurRahmanAtik <atikahmedtanvir80@gmail.com>
2024-04-16 19:08:28 +0600

    initial setup compelte

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold ">
+      Hello world!
+    </h1>
+      
+    </>
+  )
+}
+
+export default App
